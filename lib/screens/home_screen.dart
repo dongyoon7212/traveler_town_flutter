24eeb9f475e1ea8fa5d7cd322f53417843488758
dongyoon7212 <dongyoon7212@naver.com>
2024-07-14 22:41:00 +0900

    Feat: App Bar 검색 레이아웃 구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,96 +1,97 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-final List<String> imgList = [
-  "assets/banner1.jpg",
-  "assets/banner2.jpeg",
-  "assets/banner3.jpeg",
-];
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final PageController _pageController = PageController();
-  int _currentPage = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
-      if (_currentPage < imgList.length - 1) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
-      _pageController.animateToPage(
-        _currentPage,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeIn,
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              stretch: true,
-              backgroundColor: const Color.fromRGBO(22, 68, 113, 1),
-              flexibleSpace: FlexibleSpaceBar(
-                expandedTitleScale: 2,
-                title: const Text(
-                  "Traveler Town",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  textAlign: TextAlign.start,
-                ),
-                centerTitle: true,
-                collapseMode: CollapseMode.parallax,
-                background: PageView.builder(
-                  controller: _pageController,
-                  itemCount: imgList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Image.asset(
-                      imgList[index],
-                      fit: BoxFit.cover,
-                    );
-                  },
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 50,
+                child: Image.asset(
+                  "assets/logo.png",
+                  fit: BoxFit.cover,
                 ),
               ),
+              SizedBox(
+                width: 240,
+                height: 38,
+                child: TextField(
+                  textAlign: TextAlign.start,
+                  textAlignVertical: TextAlignVertical.center,
+                  cursorColor: const Color.fromRGBO(22, 68, 113, 1),
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                  cursorHeight: 20,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    prefixIcon: const Icon(
+                      Icons.search_rounded,
+                      size: 27,
+                    ),
+                    hintText: "여행할 국가를 검색해보세요!",
+                    fillColor: Colors.grey.withOpacity(0.3),
+                    filled: true,
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          color: Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        )),
+                    disabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          color: Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        )),
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          color: Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        )),
+                    border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          color: Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        )),
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.bookmark_border_outlined,
+                  size: 30,
+                ),
+              )
+            ],
+          ),
+          backgroundColor: Colors.white,
+          shape: const Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+              width: 1,
             ),
-          ];
-        },
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text("Item $index", textAlign: TextAlign.center),
-            );
-          },
-        ),
-      ),
+          )),
     );
   }
 }
-
-// void main() => runApp(const MaterialApp(home: HomeScreen()));
