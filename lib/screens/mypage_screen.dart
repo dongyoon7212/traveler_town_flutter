100059742f318d2d603f573fa0d38c85e9bf08bf
dongyoon7212 <dongyoon7212@naver.com>
2024-07-17 22:33:49 +0900

    Feat: 데이터 연동중

--- lib/screens/mypage_screen.dart
+++ lib/screens/mypage_screen.dart
@@ -2,32 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:traveler_town/models/principal_model.dart';
 import 'package:traveler_town/services/auth_api_service.dart';
 
-class Mypage extends StatefulWidget {
-  const Mypage({
-    super.key,
-  });
-
-  @override
-  State<Mypage> createState() => _MypageState();
-}
-
-class _MypageState extends State<Mypage> {
-  late final PrincipalModel principalModel;
-
-  @override
-  void initState() async {
-    super.initState();
-    principalModel = await AuthApiService.getPrincipal();
-  }
+class Mypage extends StatelessWidget {
+  const Mypage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text("$principalModel.nickname"),
-        ],
-      ),
+    return FutureBuilder<PrincipalModel>(
+      future: AuthApiService.getPrincipal(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData) {
+          return const Center(child: Text('No Data'));
+        } else {
+          PrincipalModel principalModel = snapshot.data!;
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(principalModel.nickname),
+                // Display other information from principalModel
+              ],
+            ),
+          );
+        }
+      },
     );
   }
 }
