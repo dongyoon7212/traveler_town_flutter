957357efee17f350c1919cce6da67920ca452ba6
dongyoon7212 <dongyoon7212@naver.com>
2024-07-17 21:31:31 +0900

    Feat: principal 확인 로직

--- lib/screens/mypage_screen.dart
+++ lib/screens/mypage_screen.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:traveler_town/models/principal_model.dart';
+import 'package:traveler_town/screens/signin_screen.dart';
+import 'package:traveler_town/services/auth_api_service.dart';
 
-class Mypage extends StatelessWidget {
+class Mypage extends StatefulWidget {
   const Mypage({super.key});
+
+  @override
+  State<Mypage> createState() => _MypageState();
+}
+
+class _MypageState extends State<Mypage> {
+  PrincipalModel? principal;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPrincipal();
+  }
+
+  Future<void> _fetchPrincipal() async {
+    try {
+      PrincipalModel fetchedPrincipal = await AuthApiService.getPrincipal();
+      if (!mounted) {
+        return; // ensures that the widget is still in the widget tree
+      }
+      setState(() {
+        principal = fetchedPrincipal;
+        _isLoading = false;
+      });
+    } catch (e) {
+      // Handle the error by navigating to the SignIn screen
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SignIn(),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
