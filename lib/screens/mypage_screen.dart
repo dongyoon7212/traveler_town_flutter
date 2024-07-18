7fd9478b8711587d412d5e29c4dfe94d580b5c67
dongyoon7212 <dongyoon7212@naver.com>
2024-07-18 16:09:51 +0900

    Feat: 네비게이션 구현

--- lib/screens/mypage_screen.dart
+++ lib/screens/mypage_screen.dart
@@ -11,19 +11,47 @@ class Mypage extends StatelessWidget {
       future: AuthApiService.getPrincipal(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
+          return Center(
+            child: Text(
+              'Error: ${snapshot.error}',
+            ),
+          );
         } else if (!snapshot.hasData) {
-          return const Center(child: Text('No Data'));
+          return const Center(
+            child: Text(
+              'No Data',
+            ),
+          );
         } else {
           PrincipalModel principalModel = snapshot.data!;
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(principalModel.nickname),
-                // Display other information from principalModel
+                SizedBox(
+                  width: 150,
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: const Color(0xffdbdbdb),
+                      ),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Image.network(
+                      principalModel.profileImg,
+                    ),
+                  ),
+                ),
+                _gap(),
+                Text(
+                  principalModel.nickname,
+                ),
               ],
             ),
           );
@@ -31,4 +59,6 @@ class Mypage extends StatelessWidget {
       },
     );
   }
+
+  Widget _gap() => const SizedBox(height: 16);
 }
